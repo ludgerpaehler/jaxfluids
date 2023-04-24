ba6e8f9b1c4abf891b6e1ad67384587c54f20cef
Ludger Paehler <ludger.paehler@tum.de>
2023-04-24 15:40:28 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,6 @@ curl micro.mamba.pm/install.sh | bash
 
 conda init --all
 micromamba shell init -s bash
-micromamba env create -f environment.yml --yes
+micromamba env create -f environment.yaml --yes
 # Note that `micromamba activate jaxfluids-env` doesn't work, it must be run by the
 # user (same applies to `conda activate`)
